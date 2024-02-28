5ff80b3870808e2dbf76b60a222759151e629d2e
Elton Saravia <ryuzaki12elton@gmail.com>
2024-02-28 21:15:38 +0100

    boton de cerrrar jdialog funcionando

--- Frontend/Ring/src/main/java/Registro.java
+++ Frontend/Ring/src/main/java/Registro.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Date;
+import java.util.Calendar;
 
 public class Registro extends JDialog {
 
@@ -8,6 +10,14 @@ public class Registro extends JDialog {
     private JTextField textFieldUsuario;
     private JPasswordField passwordField;
     private JPasswordField passwordFieldConfirmacion;
+    private JLabel lblNewLabelNombre;
+    private JLabel lblNewLabelApellido;
+    private JLabel lblContrasea;
+    private JLabel lblConfirmacionDeContrasea;
+    private JTextField textFieldApellidos;
+    private JTextField textFieldApodo;
+    private JTextField textFieldCorreoElectronico;
+    private JButton closeButton;
 
     public Registro(Frame owner) {
         super(owner);
@@ -21,23 +31,41 @@ public class Registro extends JDialog {
         contenedorRegistro.setLayout(null);
         contenedorRegistro.setBackground(new Color(82, 82, 82));
         setContentPane(contenedorRegistro);
+        //boton para cerrar
+        closeButton = new JButton((Icon) null);
+        closeButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		setVisible(false);; // Esto cerrará la aplicación
+        	}
+        });
+        closeButton.setBounds(1331, 11, 45, 37);
+        contenedorRegistro.add(closeButton);
+        
+        JCheckBox newCheckBoxSolicitarPermisos = new JCheckBox("Solicitar permisos para subir juegos");
+        newCheckBoxSolicitarPermisos.setFont(new Font("Tahoma", Font.BOLD, 14));
+        newCheckBoxSolicitarPermisos.setForeground(new Color(201, 157, 24));
+        newCheckBoxSolicitarPermisos.setBackground(new Color(255, 255, 255));
+        newCheckBoxSolicitarPermisos.setBounds(65, 626, 300, 23);
+        contenedorRegistro.add(newCheckBoxSolicitarPermisos);
 
         // Agrega aquí los componentes para el registro, por ejemplo:
         textFieldUsuario = new JTextField();
-        textFieldUsuario.setBounds(50, 100, 300, 30);
+        textFieldUsuario.setBounds(65, 78, 300, 30);
         contenedorRegistro.add(textFieldUsuario);
 
         passwordField = new JPasswordField();
-        passwordField.setBounds(50, 150, 300, 30);
+        passwordField.setBounds(65, 472, 300, 30);
         contenedorRegistro.add(passwordField);
 
         passwordFieldConfirmacion = new JPasswordField();
-        passwordFieldConfirmacion.setBounds(50, 200, 300, 30);
+        passwordFieldConfirmacion.setBounds(65, 565, 300, 30);
         contenedorRegistro.add(passwordFieldConfirmacion);
 
         // Botón para registrar al usuario
         JButton btnRegistrar = new JButton("Registrar");
-        btnRegistrar.setBounds(50, 250, 100, 30);
+        btnRegistrar.setFont(new Font("Tahoma", Font.BOLD, 17));
+        btnRegistrar.setForeground(new Color(201, 157, 24));
+        btnRegistrar.setBounds(65, 686, 300, 30);
         btnRegistrar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // Lógica de registro
@@ -45,32 +73,77 @@ public class Registro extends JDialog {
         });
         contenedorRegistro.add(btnRegistrar);
         
-        JPanel panel = new JPanel();
-        panel.setBounds(181, 359, 56, 43);
-        contenedorRegistro.add(panel);
+        lblNewLabelNombre = new JLabel("Nombre");
+        lblNewLabelNombre.setForeground(new Color(201, 157, 24));
+        lblNewLabelNombre.setFont(new Font("Sylfaen", Font.BOLD, 18));
+        lblNewLabelNombre.setBounds(65, 49, 300, 25);
+        contenedorRegistro.add(lblNewLabelNombre);
+        
+        lblNewLabelApellido = new JLabel("Apellidos");
+        lblNewLabelApellido.setForeground(new Color(201, 157, 24));
+        lblNewLabelApellido.setFont(new Font("Sylfaen", Font.BOLD, 18));
+        lblNewLabelApellido.setBounds(65, 124, 300, 25);
+        contenedorRegistro.add(lblNewLabelApellido);
+        
+        lblContrasea = new JLabel("Contraseña");
+        lblContrasea.setForeground(new Color(201, 157, 24));
+        lblContrasea.setFont(new Font("Sylfaen", Font.BOLD, 18));
+        lblContrasea.setBounds(65, 448, 300, 25);
+        contenedorRegistro.add(lblContrasea);
+        
+        lblConfirmacionDeContrasea = new JLabel("Confirmacion de contraseña");
+        lblConfirmacionDeContrasea.setForeground(new Color(201, 157, 24));
+        lblConfirmacionDeContrasea.setFont(new Font("Sylfaen", Font.BOLD, 18));
+        lblConfirmacionDeContrasea.setBounds(65, 534, 300, 25);
+        contenedorRegistro.add(lblConfirmacionDeContrasea);
+        
+        textFieldApellidos = new JTextField();
+        textFieldApellidos.setBounds(65, 148, 300, 30);
+        contenedorRegistro.add(textFieldApellidos);
+        
+        JLabel lblNewLabelApodo = new JLabel("Apodo");
+        lblNewLabelApodo.setForeground(new Color(201, 157, 24));
+        lblNewLabelApodo.setFont(new Font("Sylfaen", Font.BOLD, 18));
+        lblNewLabelApodo.setBounds(65, 368, 300, 25);
+        contenedorRegistro.add(lblNewLabelApodo);
+        
+        textFieldApodo = new JTextField();
+        textFieldApodo.setBounds(65, 388, 300, 30);
+        contenedorRegistro.add(textFieldApodo);
+        
+        textFieldCorreoElectronico = new JTextField();
+        textFieldCorreoElectronico.setBounds(65, 308, 300, 30);
+        contenedorRegistro.add(textFieldCorreoElectronico);
+        
+        JLabel lblCorreoElectronico = new JLabel("Correo Electronico");
+        lblCorreoElectronico.setForeground(new Color(201, 157, 24));
+        lblCorreoElectronico.setFont(new Font("Sylfaen", Font.BOLD, 18));
+        lblCorreoElectronico.setBounds(65, 282, 300, 25);
+        contenedorRegistro.add(lblCorreoElectronico);
+        
+        JComboBox comboBoxEdad = new JComboBox();
+        comboBoxEdad.setModel(new DefaultComboBoxModel(new String[] {"17", "18", "19"}));
+        comboBoxEdad.setBounds(65, 213, 300, 30);
+        contenedorRegistro.add(comboBoxEdad);
+        
+        JLabel lblNewLabelEdad = new JLabel("Edad");
+        lblNewLabelEdad.setForeground(new Color(201, 157, 24));
+        lblNewLabelEdad.setFont(new Font("Sylfaen", Font.BOLD, 18));
+        lblNewLabelEdad.setBounds(65, 189, 300, 25);
+        contenedorRegistro.add(lblNewLabelEdad);
+        
+        JLabel BackgroundRegistro = new JLabel("New label");
+        BackgroundRegistro.setIcon(new ImageIcon(Registro.class.getResource("/imagenes/ImagenRegistro.jpg")));
+        BackgroundRegistro.setBounds(0, 0, 1401, 863);
+        contenedorRegistro.add(BackgroundRegistro);
 
         // Repite la lógica de los botones de minimizar y cerrar como en tu JFrame principal si es necesario
 
-        // Añadir un fondo o cualquier otro componente que requieras
-        JButton closeButton = new JButton(new ImageIcon(PantallaPrincipal.class.getResource("/imagenes/cerrar (1).png")));
-		closeButton.setBounds(1336, 10, 45, 37); // Ajusta la posición y el tamaño según sea necesario
-		closeButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0); // Esto cerrará la aplicación
-			}
-		});
-		
-		JButton minimizeButton = new JButton(new ImageIcon("path_to_minimize_icon.png"));
-		minimizeButton.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {
-		        // Para JDialog, solo oculta el diálogo. No se minimiza en la barra de tareas.
-		        setVisible(false);
-		    }
-		});
+
 		
 			
 		
-		minimizeButton.setBounds(1281, 10, 45, 37);
+		
     }
 
     // Método para mostrar el diálogo de registro
