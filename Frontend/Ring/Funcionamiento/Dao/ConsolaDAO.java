ac978a1d2dc2180690f79d2cc2bd0675ba7bc356
AlexGon8 <0224agonzalez@e-itaca.es>
2024-03-11 09:57:32 +0100

    avance en los Dao

--- Frontend/Ring/Funcionamiento/Dao/ConsolaDAO.java
+++ Frontend/Ring/Funcionamiento/Dao/ConsolaDAO.java
@@ -1,5 +1,104 @@
 package Dao;
 
-public class ConsolaDAO {
+import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import Dao.IDao;
+import tables.Educacion;
+import util.HibernateUtil;
+
+public class ConsolaDAO implements IDao<Consola, Integer>{
+	
+	@Override
+	public boolean crear(Consola co) {
+		try (Session session = HibernateUtil.getSessionFactory().openSession()){
+			Transaction tx = session.beginTransaction();
+			
+			session.persist(ed);
+			tx.commit();	
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	@Override
+	public Consola leer(Integer co_id) {
+		Consola co = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()){
+			String hql = "from consola where id= :co_id" ;
+	        Query query = session.createQuery(hql,Consola.class);
+	        query.setParameter("invId", co_id);
+	        query.setMaxResults(1);
+			ed = (Consola)  query.getSingleResult();					
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		return co;	
+	}
+
+	@Override
+	public boolean actualizar(Consola modelo, Integer id) {
+	    try (Session session = HibernateUtil.getSessionFactory().openSession()){
+	        session.beginTransaction();
+	        
+	        Consola educacionPersistente = session.get(Consola.class, id);
+	        if (educacionPersistente != null) {
+	            // Actualizar los campos de la educación persistente con los valores del modelo
+	            educacionPersistente.setEdNombre(modelo.getEdNombre());
+	            educacionPersistente.setEdGradoe(modelo.getEdGradoe());
+	            educacionPersistente.setEdTermino(modelo.getEdTermino());
+	            
+	            session.merge(educacionPersistente);
+	            
+	            session.getTransaction().commit();
+	            return true;
+	        } else {
+	            // No se encontró la educación con el id proporcionado
+	            return false;
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return false;
+	    }
+	}
+
+	@Override
+	public boolean eliminar(Integer id) {
+	    try (Session session = HibernateUtil.getSessionFactory().openSession()){
+	        session.beginTransaction();
+	        
+	        Educacion educacion = session.get(Educacion.class, id);
+	        if (educacion != null) {
+	            session.remove(educacion);
+	            session.getTransaction().commit();
+	            return true;
+	        } else {
+	            // No se encontró la educación con el id proporcionado
+	            return false;
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return false;
+	    }
+	}
+
+	@Override
+	public List<Educacion> listar() {
+	    List<Educacion> listaEducaciones = null;
+	    try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+	        String hql = "FROM educacion";
+	        Query<Educacion> query = session.createQuery(hql, Educacion.class);
+	        listaEducaciones = query.getResultList();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return listaEducaciones;
+	}
+
+	
 }
---
+++ Frontend/Ring/Funcionamiento/Dao/IDao.java
@@ -0,0 +1,16 @@
+package Dao;
+
+import java.util.List;
+
+public interface IDao<T, V> {
+
+	public boolean crear(T modelo);
+
+	public T leer(V id);
+
+	public boolean actualizar(T modelo, V id);
+
+	public boolean eliminar(V id);
+
+	public List<T> listar();
+}
